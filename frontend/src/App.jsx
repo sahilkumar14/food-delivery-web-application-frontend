898267265da4cd97a030c1cd83bc6ed0f40ef9e5
SaniyaGupta6 <gsanya029@gmail.com>
2026-03-31 14:11:25 +0530

    [Saniya].frontend added homepage-restaurantlist

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import restaurantsData from "./data/restaurants";
+
+// components
+import Hero from "./components/Hero";
+import CategorySection from "./components/CategorySection";
+import FeaturedRestaurants from "./components/FeaturedRestaurants";
 
 function App() {
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [filteredData, setFilteredData] = useState(restaurantsData);
+
+  // ✅ ADD HERE (INSIDE App, BEFORE return)
+  const handleSearch = () => {
+    const result = restaurantsData.filter((item) => {
+
+      const matchesSearch =  item.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+  item.cuisine.toLowerCase().includes(searchQuery.toLowerCase());
+
+      const matchesCategory =
+        selectedCategory === "all" ||
+        item.category.toLowerCase().includes(selectedCategory);
+
+      return matchesSearch && matchesCategory;
+    });
+
+    setFilteredData(result);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [searchQuery, selectedCategory]);
+
   return (
-    <div>homepage</div>
-  )
+    <div>
+
+      {/* 🔍 HERO (SEARCH BAR) */}
+      <Hero
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+        handleSearch={handleSearch}
+      />
+
+      {/* 🍔 CATEGORY */}
+      <CategorySection setSelectedCategory={setSelectedCategory} />
+
+      {/* 🍽️ RESTAURANTS */}
+      <FeaturedRestaurants data={filteredData} />
+
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Hero from "../components/Hero";
+import CategorySection from "../components/CategorySection";
+import FeaturedRestaurants from "../components/FeaturedRestaurants";
+
+const Home = () => {
+  return (
+    <div>
+
+      <Hero />
+
+      <CategorySection />
+
+      <FeaturedRestaurants />
+
+    </div>
+  );
+};
+
+export default Home;
